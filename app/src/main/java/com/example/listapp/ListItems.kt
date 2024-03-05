7ec16dba23d3b2a1cdc112e559239326f6d87911
Shubham_Kumar <shubham124285@gmail.com>
2024-03-05 17:01:37 +0530

    lazy column 1

--- app/src/main/java/com/example/listapp/ListItems.kt
+++ app/src/main/java/com/example/listapp/ListItems.kt
@@ -1,5 +1,7 @@
 package com.example.listapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -61,6 +65,7 @@ fun LoadItems(){
                 .padding(14.dp)
         ){
             items(sItems){
+                ManageItems(item = it, {}, {})
 
             }
 
@@ -121,8 +126,30 @@ fun LoadItems(){
 
 }
 
+
+@Composable
+fun ManageItems(
+    item:ListItem,
+    onEditClick:()->Unit,
+    onDeleteClick:()->Unit){
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(border = BorderStroke(2.dp, Color.Cyan), shape = RoundedCornerShape(20))
+    ) {
+        Text(text = item.itemName, modifier = Modifier.padding(8.dp))
+
+
+    }
+
+
+}
+
 @Composable
 @Preview(showBackground = true)
 fun LoadItemsPreview(){
     LoadItems()
+
 }
